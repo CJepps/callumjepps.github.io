e7fedc54e46f7cd862dead6c2e09dfb6408f502b
CJepps <cjepps@hotmail.co.uk>
2022-09-30 13:45:39 +0000

    Added hero text and CTA button to homepage.

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,11 +1,19 @@
-import './index.scss';
+import './index.scss'
+import { Link } from 'react-router-dom'
+import LogoCapital from '../../assets/images/logo-capital-c.png'
 const Home = () => {
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm</h1>
-                <img alt="imgae of my initial"></img>
+                <h1>Hi, <br /> I'm
+                <img src={LogoCapital} alt="imgae of my initial"></img>
+                allum Jepps
+                <br />
+                Web Developer
+                </h1>
+                <h2>Fullstack Developer / DJ</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
         </div>
     );
