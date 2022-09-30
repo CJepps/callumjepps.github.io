0275e9b3b6016f3968220936d26e1f6f2b9a22e7
CJepps <cjepps@hotmail.co.uk>
2022-09-30 10:45:27 +0000

    Fixed routing syntax error.

---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+const Home = () => {
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm</h1>
+                <img alt="imgae of my initial"></img>
+            </div>
+        </div>
+    );
+}
+
+export default Home
