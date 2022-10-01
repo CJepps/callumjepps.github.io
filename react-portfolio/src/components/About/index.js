9ad2244ad5062e8762e9c687d0f52b563cb57d7f
CJepps <cjepps@hotmail.co.uk>
2022-10-01 11:01:21 +0000

    Added contact form to about me page with styling.

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,24 +1,41 @@
+import './index.scss'
 import AnimatedLetters from '../AnimatedLetters';
-import './index.scss';
+import { useEffect, useState } from 'react';
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        
+        let timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+        
+        return () => {
+                    clearTimeout(timeoutId)
+                }
+    }, [])
 
     return (
-        <div className="container about-page">
-            <div className="text-zone ">
-                <h1>
-                    <AnimatedLetters 
-                        strArray={"About me".split("")}
-                        idx={15}
-                    />
-                </h1>
-                <p>Text about me being the greatestText about me being the greatestText about me being the greatest</p>
-                <p>Text about me being the greatestText about me being the greatestText about me being the greatest</p>
-                <p>Text about me being the greatestText about me being the greatestText about me being the greatest</p>
+        <>
+            <div className="container contact-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters 
+                            letterClass={letterClass}
+                            strArray={"About me".split("")}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>Text about me being the greatest Text about me being the greatest Text about me being the greatest</p>
+                    <p>Text about me being the greatest Text about me being the greatest Text about me being the greatest</p>
+                    <p>Text about me being the greatest Text about me being the greatest Text about me being the greatest</p>
 
+
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
-export default About 
+export default About
--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -27,7 +27,31 @@ const Contact = () => {
                             idx={15}
                         />
                     </h1>
-
+                    <p>Please dont hesitate to contact me on the below form.
+                       I am keen to help with any project to increase my skills
+                       and would love to hear from you! 
+                    </p>
+                    <div className="contact-form">
+                        <form>
+                            <ul>
+                                <li className='half'>
+                                    <input type="text" name="name" placeholder='Name' required />
+                                </li>
+                                <li className='half'>
+                                    <input type="email" name="email" placeholder='Email' required />
+                                </li>
+                                <li >
+                                    <input type="text" name="subject" placeholder='subject' required />
+                                </li>
+                                <li>
+                                    <textarea placeholder='Your message here...' name='message' required />
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
                 </div>
             </div>
         </>
