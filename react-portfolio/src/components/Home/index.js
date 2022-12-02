542572d12d9e535c2bca91990132f2d9a5d383d8
CJepps <cjepps@hotmail.co.uk>
2022-12-02 20:15:47 +0000

    ammended hompage property.

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -6,46 +6,50 @@ import LogoCapital from '../../assets/images/logo-capital-c.png'
 import Logo from './Logo'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = 'allum Jepps,'.split('')
+  const jobArray = 'web developer'.split('')
 
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = "allum Jepps".split("")
-    const jobArray = "web developer".split("")
+  useEffect(() => {
+    let timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
 
-    useEffect(() => {
-        
-        let timeoutId = setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 4000)
-        
-        return () => {
-                    clearTimeout(timeoutId)
-                }
-    }, [])
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
 
-    return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={letterClass} >H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
-                    <br /> 
-                    <span className={`${letterClass} _12`}>I</span>
-                    <span className={`${letterClass} _12`}>'m</span>
-                    <img src={LogoCapital} alt="Logo of initials"></img>
-                    <AnimatedLetters letterClass={letterClass}
-                    strArray={nameArray}
-                    idx={15} />
-                    <br />
-                    <AnimatedLetters letterClass={letterClass}
-                    strArray={jobArray}
-                    idx={22} />
-                </h1>
-                <h2>Fullstack Developer / DJ</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-            </div>
-            <Logo />
-        </div>
-    );
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _12`}>I</span>
+          <span className={`${letterClass} _12`}>'m</span>
+          <img src={LogoCapital} alt="Logo of initials"></img>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Fullstack Developer / DJ</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+      <Logo />
+    </div>
+  )
 }
 
 export default Home
